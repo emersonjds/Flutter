8744dad685c12f5124eb6725c66225a35db14865
Emerson Silva <emerson_jdss@hotmail.com>
2020-06-11 12:26:38 -0300

    loops

---
+++ flutter_starter/dart/loops.dart
@@ -0,0 +1,24 @@
+void main() {
+
+  for(var i = 0; i < 10; i++) {
+    print("$i");
+  }
+
+  for(var i = 100; i >= 10; i -= 5) {
+    print("$i");
+  }
+
+  int j = 0;
+
+  while(j <= 10) {
+    print("$j");
+    j++;
+  }
+
+  int k = 0;
+  do {
+    print(k);
+    k++;
+  } while (k <= 10);
+
+}
