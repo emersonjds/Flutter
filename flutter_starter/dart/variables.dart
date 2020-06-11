5b0e9e714470f0af9ca8165ae3ee590cb49ea6e2
Emerson Silva <emerson_jdss@hotmail.com>
2020-06-11 11:46:25 -0300

    dart variables

---
+++ flutter_starter/dart/variables.dart
@@ -0,0 +1,23 @@
+void main() {
+  //store
+  String name = "Loja";
+  int fruits = 5;
+  double price = 10.5;
+  bool hasFruit = true;
+
+  print("O nome escrito é $name");
+  print(fruits);
+
+  print("A $name tem $fruits frutas disponiveis");
+
+  var value = 1;
+  print(value);
+  value = "Daniel";
+  print(value);
+
+  dynamic value2 = 1;
+  print(value2);
+  value2 = "Testing";
+  print(value2);
+
+}
